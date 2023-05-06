5dc33fb4c19eb4af921d259207f66000a0c494c2
Hongrok Lim <hongrr123@gmail.com>
2023-05-06 23:59:09 +0900

    Init Repo

---
+++ src/main/java/dev/rokong/dbgraphgen/DbGraphGenApp.java
@@ -0,0 +1,11 @@
+package dev.rokong.dbgraphgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbGraphGenApp {
+	public static void main(String[] args) {
+		SpringApplication.run(DbGraphGenApp.class, args);
+	}
+}
---
+++ src/test/java/dev/rokong/dbgraphgen/DbGraphGenAppTests.java
@@ -0,0 +1,14 @@
+package dev.rokong.dbgraphgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbGraphGenAppTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
